39f98f880dc21a9cb13c300460abb2087c12a77f
Phanisekhar <pani_sekr@yahoo.co.in>
2023-04-18 18:48:13 +0530

    Removed Asp.Net Core WEB and web api

--- PEI_ETL_MetaDataProcessWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PEI_ETL_MetaDataProcessWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
